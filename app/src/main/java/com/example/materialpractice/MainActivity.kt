4aef091cab77a1c3ddd08d847e6c56ed7adbb0c4
heesungum <heesungum20@gmail.com>
2022-03-04 16:21:40 +0900

    Add TextFieldFragment

--- app/src/main/java/com/example/materialpractice/MainActivity.kt
+++ app/src/main/java/com/example/materialpractice/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.materialpractice
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.materialpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val textFieldFragment = TextFieldFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val bottomSheet = ModalBottomSheet()
+
+        changeFragment(textFieldFragment)
 
         binding.modalButton.setOnClickListener {
             bottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
@@ -22,4 +29,10 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    private fun changeFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.container).navigateUp()
 }
